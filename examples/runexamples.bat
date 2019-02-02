354b56a949363ec0654a2122299330e6a14c2748
misha <mike008@bk.ru>
2019-02-02 17:14:20 +0300

    Initial commit 2

---
+++ examples/runexamples.bat
@@ -0,0 +1,25 @@
+..\bin\skeleton64f.exe example.ine
+..\bin\skeleton64f.exe cwcv.ine --edges --ineinc
+..\bin\skeleton64f.exe cwcv.ext
+..\bin\skeleton64f.exe equ.ine --verifyine
+..\bin\skeleton64f.exe sf.ine --skeletonformat
+..\bin\skeleton64f.exe ucube.ine --avisfukudaformat --adjacency --facetadjacency --extinc --ineinc
+..\bin\skeleton64f.exe exvoronoi.ine --ineinc --extinc
+..\bin\skeleton64f.exe exdelaunay.ext --extinc
+
+..\bin\skeleton64f.exe ccc6.ext --avisfukudaformat
+..\bin\skeleton64f.exe cyc.ine --avisfukudaformat
+
+..\bin\skeleton64f.exe cube.ine
+..\bin\skeleton64f.exe cube.ext
+
+..\bin\skeleton64f.exe ex1.ine
+..\bin\skeleton64f.exe ex2.ine --skeletonformat
+..\bin\skeleton64f.exe ex3.ine --skeletonformat
+
+..\bin\skeleton64f.exe maximenko.ine
+
+fc golden.out\* *
+
+
+
